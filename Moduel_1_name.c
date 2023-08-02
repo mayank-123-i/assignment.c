9516ec414376377ee559a79d4cc192c557e097d8
mayank-123-i <137789141+mayank-123-i@users.noreply.github.com>
2023-08-02 15:45:29 +0530

    Add files via upload

---
+++ Moduel_1_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    printf("Name : Mayank\n");
+
+    printf("Date of birth : 28 7 2006\n");
+
+    printf("Age : 17\n");
+
+    printf("Address : Vaibhavlaxmi residency ,mota varachha,surat");
+    
+    return 0;
+}
